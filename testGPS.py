4198c53fdf766307ac19e953e651425d08aeadd7
HasrulAzim <hasrul.azim@visi.com.my>
2023-03-21 17:42:55 +0800

    Update testGPS.py

--- testGPS.py
+++ testGPS.py
@@ -42,9 +42,9 @@ def connect_mqtt():
     client.on_connect = on_connect
     client.on_message = on_message
     client.connect(broker, port)
+    client.loop_start
     client.subscribe("/visi/amantronic/rs/command/startLog")
     client.subscribe("/visi/amantronic/rs/command/filename")
-    client.loop_start
     return client
         
 def publish(client, topic, msg):

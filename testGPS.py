bcb427d2ee1c5aa7a4f3b40dfe79643bfefd7c16
HasrulAzim <hasrul.azim@visi.com.my>
2023-03-21 17:16:28 +0800

    Update testGPS.py

--- testGPS.py
+++ testGPS.py
@@ -51,7 +51,8 @@ def publish(client, topic, msg):
     result = client.publish(topic, msg)
     status = result[0]
     if status == 0:
-        print(f"Send `{msg}` to topic `{topic}`")
+        print(".")
+ #       print(f"Send `{msg}` to topic `{topic}`")
     else:
         print(f"Failed to send message to topic {topic}")
     return result
